c2773d921a2b65e0ba37ec556abe21ad969ea640
maryli891 <marytta095@gmail.com>
2017-10-31 19:46:09 -0300

    Se creo clase rectangulo y el ejercicio Area Rectangulo

--- src/evajava1/Rectangulo.java
+++ src/evajava1/Rectangulo.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package evajava1;
-
+import java.util.Scanner;//PASO 1
 /**
  *
  * @author LC22
  */
-public class Rectangulo {
+public class Rectangulo 
+{
+ public static void main(String[] args) 
+ {
+   float area, base, altura;// PASO 2
+   Scanner sc=new Scanner(System.in);//PASO 3
+   //PASO 4
+   System.out.println("Ingrese la base del rectangulo");
+    base =sc.nextFloat();//Leo y guardo los datos de entrada
+    
+    System.out.println("Ingrese la altura del rectangulo");
+        altura =sc.nextFloat(); 
+        //PASO 5
+        area=base*altura;
+        System.out.println(" El area del rectangulo es: "+area+" mts2"); 
+ }
     
 }
